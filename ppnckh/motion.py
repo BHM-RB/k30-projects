15b564596127db62c740d067afdd09952c2c3c9e
BHM-RB <bhm.rb27@gmail.com>
2021-04-20 23:31:05 +0700

    update ppnckh

--- ppnckh/motion.py
+++ ppnckh/motion.py
@@ -5,32 +5,32 @@ import cv2
 default_expanded_size = 10
 
 
-def add_size(input_size, max_size):
-    new_size = input_size + default_expanded_size
-    return input_size if new_size > max_size else new_size
+def get_emotion(img_path):
+    def add_size(input_size, max_size):
+        new_size = input_size + default_expanded_size
+        return input_size if new_size > max_size else new_size
 
+    def remove_size(input_size):
+        new_size = input_size - default_expanded_size
+        return input_size if new_size < 0 else new_size
 
-def remove_size(input_size):
-    new_size = input_size - default_expanded_size
-    return input_size if new_size < 0 else new_size
+    img = cv2.imread(img_path)
+    max_height = img.shape[0]
+    max_width = img.shape[1]
+    face_detector = MTCNN()
+    emotion_detector = FER()
+    faces = face_detector.detect_faces(img)
 
+    for face in faces:
+        x, y, w, h = face['box']
+        face_img = img[remove_size(y): add_size(y + h, max_height), remove_size(x): add_size(x + w, max_width)]
 
-img = cv2.imread("smile.jpg")
-max_height = img.shape[0]
-max_width = img.shape[1]
-face_detector = MTCNN()
-emotion_detector = FER()
-faces = face_detector.detect_faces(img)
+        emotion_detector.detect_emotions(face_img)
+        emotion, score = emotion_detector.top_emotion(face_img)
+        #TODO: count emotion
+        print(emotion)
 
-for face in faces:
-    x, y, w, h = face['box']
-    face_img = img[remove_size(y): add_size(y + h, max_height), remove_size(x): add_size(x + w, max_width)]
-
-    emotion_detector.detect_emotions(face_img)
-    emotion, score = emotion_detector.top_emotion(face_img)
-    print(emotion)
-
-
-def get_emotion(img):
-    #TODO: read img, return emotion
     return emotion
+
+#TODO: read file name in folder "resource"
+get_emotion("smile.jpg")
--- ppnckh/primer_pitches.py
+++ ppnckh/primer_pitches.py
@@ -1,36 +1,32 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img = cv2.imread('smile.jpg')
-height, width, channels = img.shape
+def get_primer_pitches(img_path):
+    img = cv2.imread(img_path)
+    height, width, channels = img.shape
 
-new_w = int(width/3)
-# imgplot = plt.imshow(cv2.cvtColor(img[0:height, 0:new_w], cv2.COLOR_BGR2RGB))
-# plt.show()
-# imgplot = plt.imshow(cv2.cvtColor(img[0:height, new_w:new_w*2], cv2.COLOR_BGR2RGB))
-# plt.show()
-# imgplot = plt.imshow(cv2.cvtColor(img[0:height, new_w*2:new_w*3], cv2.COLOR_BGR2RGB))
-# plt.show()
-# cv2.imwrite('1.png', img[0:height, 0:new_w])
-# cv2.imwrite('2.png', img[0:height, new_w:new_w*2])
-# cv2.imwrite('3.png', img[0:height, new_w*2:new_w*3])
+    new_w = int(width / 3)
+    # imgplot = plt.imshow(cv2.cvtColor(img[0:height, 0:new_w], cv2.COLOR_BGR2RGB))
+    # plt.show()
+    # imgplot = plt.imshow(cv2.cvtColor(img[0:height, new_w:new_w*2], cv2.COLOR_BGR2RGB))
+    # plt.show()
+    # imgplot = plt.imshow(cv2.cvtColor(img[0:height, new_w*2:new_w*3], cv2.COLOR_BGR2RGB))
+    # plt.show()
+    # cv2.imwrite('1.png', img[0:height, 0:new_w])
+    # cv2.imwrite('2.png', img[0:height, new_w:new_w*2])
+    # cv2.imwrite('3.png', img[0:height, new_w*2:new_w*3])
 
-hsv1 = cv2.cvtColor(img[0:height, 0:new_w], cv2.COLOR_BGR2HSV)
-pitch1 = int(hsv1[..., 2].mean()/255*127)
-print(pitch1)
+    hsv1 = cv2.cvtColor(img[0:height, 0:new_w], cv2.COLOR_BGR2HSV)
+    pitch1 = int(hsv1[..., 2].mean() / 255 * 127)
 
-hsv1 = cv2.cvtColor(img[0:height, new_w:new_w*2], cv2.COLOR_BGR2HSV)
-pitch2 = int(hsv1[..., 2].mean()/255*127)
-print(pitch2)
+    hsv1 = cv2.cvtColor(img[0:height, new_w:new_w * 2], cv2.COLOR_BGR2HSV)
+    pitch2 = int(hsv1[..., 2].mean() / 255 * 127)
 
-hsv1 = cv2.cvtColor(img[0:height, new_w*2:new_w*3], cv2.COLOR_BGR2HSV)
-pitch3 = int(hsv1[..., 2].mean()/255*127)
-print(pitch3)
+    hsv1 = cv2.cvtColor(img[0:height, new_w * 2:new_w * 3], cv2.COLOR_BGR2HSV)
+    pitch3 = int(hsv1[..., 2].mean() / 255 * 127)
 
-primer_pitches='[' + str(pitch1) + ',' + str(pitch2) + ',' + str(pitch3) + ']'
-print(primer_pitches)
-
-
-def get_primer_pitches(img):
-    #TODO: read img, return primer_pitches
+    primer_pitches = '[' + str(pitch1) + ',' + str(pitch2) + ',' + str(pitch3) + ']'
     return primer_pitches
+
+#TODO: read file name in folder "resource"
+print(get_primer_pitches('smile.jpg'))
