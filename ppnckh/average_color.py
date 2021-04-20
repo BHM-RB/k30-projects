6d98d49a3ea4d7b6e1bd098767887e0390e87b4e
BHM-RB <bhm.rb27@gmail.com>
2021-04-20 21:42:39 +0700

    update ppnchk

--- ppnckh/average_color.py
+++ ppnckh/average_color.py
@@ -11,15 +11,21 @@ new_w = int(width/3)
 # plt.show()
 # imgplot = plt.imshow(cv2.cvtColor(img[0:height, new_w*2:new_w*3], cv2.COLOR_BGR2RGB))
 # plt.show()
-cv2.imwrite('1.png', img[0:height, 0:new_w])
-cv2.imwrite('2.png', img[0:height, new_w:new_w*2])
-cv2.imwrite('3.png', img[0:height, new_w*2:new_w*3])
+# cv2.imwrite('1.png', img[0:height, 0:new_w])
+# cv2.imwrite('2.png', img[0:height, new_w:new_w*2])
+# cv2.imwrite('3.png', img[0:height, new_w*2:new_w*3])
 
 hsv1 = cv2.cvtColor(img[0:height, 0:new_w], cv2.COLOR_BGR2HSV)
-print(int(hsv1[..., 2].mean()/255*127))
+pitch1 = int(hsv1[..., 2].mean()/255*127)
+print(pitch1)
 
 hsv1 = cv2.cvtColor(img[0:height, new_w:new_w*2], cv2.COLOR_BGR2HSV)
-print(int(hsv1[..., 2].mean()/255*127))
+pitch2 = int(hsv1[..., 2].mean()/255*127)
+print(pitch2)
 
 hsv1 = cv2.cvtColor(img[0:height, new_w*2:new_w*3], cv2.COLOR_BGR2HSV)
-print(int(hsv1[..., 2].mean()/255*127))
+pitch3 = int(hsv1[..., 2].mean()/255*127)
+print(pitch3)
+
+primer_pitches='[' + str(pitch1) + ',' + str(pitch2) + ',' + str(pitch3) + ']'
+print(primer_pitches)
